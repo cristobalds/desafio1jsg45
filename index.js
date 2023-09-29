0cba0f7b72b72ecfc2038e73e4b55049e4c20485
cristobal duran <cristobalds138@gmail.com>
2023-09-29 04:45:55 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var precio = 400000;
+var cantidad = 0;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+cantidadSpan = document.querySelector(".cantidad");
+cantidadSpan.innerHTML = cantidad;
+
+valorTotal = document.querySelector('.valor-total');
+valorTotal.innerHTML = 0;
